5bdfd8128159f158c80398faa376416cde71224c
Oma3r3 <114656884+Oma3r3@users.noreply.github.com>
2023-11-02 19:51:07 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host: 'localhost',
+    host: 'con',
     database: 'nodejsproject',
     user: 'root',
     password: 'Omarlm10a$',
